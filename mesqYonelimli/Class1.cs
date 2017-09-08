cfc334592e01cc3232580c95d4b2b72037fd4aa4
Cavidan <acavidan.p102@code.edu.az>
2017-09-09 00:59:01 +0400

    first commit

---
+++ mesqYonelimli/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mesqYonelimli
+{
+    class Class1
+    {
+        public int eded1;
+        public int eded2;
+        public char simvol;
+
+        public Class1(int _eded1, int _eded2, char _simvol)
+        {
+            this.eded1 = _eded1;
+            this.eded2 = _eded1;
+            this.simvol = _simvol;
+
+            if (this.simvol == '+')
+            {
+                int cem = _eded1 + _eded2;
+                Console.WriteLine("Sizin Netice " + cem);
+            }
+
+            if (this.simvol == '-')
+            {
+                int ferq = _eded1 - _eded2;
+                Console.WriteLine("Sizin Netice " + ferq);
+            }
+
+            if (this.simvol == '/')
+            {
+                int qismet = _eded1 / _eded2;
+                Console.WriteLine("Sizin Netice " + qismet);
+            }
+
+            if (this.simvol == '*')
+            {
+                int hasil = _eded1 * _eded2;
+                Console.WriteLine("Sizin Netice " + hasil);
+            }
+        }
+
+
+    }
+}
---
+++ mesqYonelimli/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mesqYonelimli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Basdan:
+            Console.WriteLine("Birinci Ededi Yazin");
+            int eded1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Simvolu Daxil Edin");
+            char smvl = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Ikinci Ededi Yazin");
+            int eded2 = Convert.ToInt32(Console.ReadLine());
+            Class1 cs1 = new Class1(eded1, eded2,smvl);
+            goto Basdan;
+
+
+        }
+    }
+}
